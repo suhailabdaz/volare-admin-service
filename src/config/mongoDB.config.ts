40156b409f05732ccc79589f3008bc43a6459c3a
suhailabdaz <suhail.abdaz@gmail.com>
2024-09-14 13:32:24 +0530

    compose_env_1

--- src/config/mongoDB.config.ts
+++ src/config/mongoDB.config.ts
@@ -4,9 +4,9 @@ import "dotenv/config";
 
 const connectDB = async () => {
     try {
-      const mongoURI = `${process.env.MONGO_URI}${process.env.MONGODB_NAME}`;
+      const mongoURI = `${process.env.ADMIN_MONGO_URI}${process.env.ADMIN_MONGODB_NAME}`;
       const conn = await mongoose.connect(
-        `${process.env.MONGO_URI}${process.env.MONGODB_NAME}`
+        `${process.env.ADMIN_MONGO_URI}${process.env.ADMIN_MONGODB_NAME}`
       );    
       console.log(`AdminDB-connected: ${conn.connection.host}`);
     } catch (error: any) {
